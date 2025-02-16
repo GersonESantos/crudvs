4f6c0fc57de1bdef50e2604c93a9d612cceaf2b0
GersonESS <gebhsantos@gmail.com>
2025-02-15 22:35:23 -0300

    a

--- 03/app.js
+++ 03/app.js
@@ -1,22 +1,9 @@
 const express = require('express');
-const mysql = require("mysql2");
 const app = express();
 
 app.get('/', (req, res) => {
   res.write('Ola!');
   res.end()
-});
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Gabibi89*",
-  database: "projeto"
-});
-
-db.connect(err => {
-  if (err) throw err;
-  console.log("Banco de dados conectado!");
 });
 
 app.listen(3000, () => {
