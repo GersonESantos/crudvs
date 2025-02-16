cb07394d9bbdc8495df56286f15289f265a1e269
GersonESS <gebhsantos@gmail.com>
2025-02-15 23:42:46 -0300

    a

--- 06/app.js
+++ 06/app.js
@@ -1,25 +1,32 @@
 const express = require('express');
-const rota_produto = require('./rotas/produtos_rotas');
-// const mysql = require("mysql2");
+ const rota_produto = require('./rotas/produtos_rotas');
+const mysql = require("mysql2");
 const app = express();
+
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Gabibi89*",
+  database: "projeto"
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log("Banco de dados conectado!");
+});
+
 
 app.get('/', (req, res) => {
   res.write('Ola!');
   res.end()
 });
-
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Gabibi89*",
-//   database: "projeto"
-// });
-
-// db.connect(err => {
-//   if (err) throw err;
-//   console.log("Banco de dados conectado!");
-// });
-
+app.get("/clientes", (req, res) => {
+  db.query("SELECT * FROM cliente", (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
--- 06/rotas/produtos_rotas.js
+++ 06/rotas/produtos_rotas.js
@@ -7,46 +7,46 @@ const router = express.Router();
 // Importar módulo de serviços
 const servico = require('../servicos/produto_servico');
 
-// ***  ROTAS 
-// Rota principal
-router.get('/', function(req, res){
-    servico.formularioCadastro(req, res);
+// // ***  ROTAS 
+// // Rota principal
+// router.get('/', function(req, res){
+//     servico.formularioCadastro(req, res);
 
-});
-// Rota principal contendo a situacao
-router.get('/:situacao', function(req, res){
-  servico.formularioCadastroComSituacao(req, res);  
+// });
+// // Rota principal contendo a situacao
+// router.get('/:situacao', function(req, res){
+//   servico.formularioCadastroComSituacao(req, res);  
       
-});
+// });
 
-router.get('/listar/:categoria', function(req, res){
- servico.listagemProdutos(req, res);   
-});
-// Rota de pesquisa
+// router.get('/listar/:categoria', function(req, res){
+//  servico.listagemProdutos(req, res);   
+// });
+// // Rota de pesquisa
 
-router.get('/pesquisa', function pesquisa(req, res){
-    servico.pesquisa(req, res);    
-}); 
+// router.get('/pesquisa', function pesquisa(req, res){
+//     servico.pesquisa(req, res);    
+// }); 
 
-//Fim da rota de pesquisa
+// //Fim da rota de pesquisa
 
-// Rota de cadastro
-router.get('/cadastrar', function(req, res){
-    servico.cadastrarProduto(req, res);
- });
-// Rota para remover produtos
-router.get('/remover/:codigo&:imagem', function(req, res){
-    servico.removerProduto(req, res);
-});
+// // Rota de cadastro
+// router.get('/cadastrar', function(req, res){
+//     servico.cadastrarProduto(req, res);
+//  });
+// // Rota para remover produtos
+// router.get('/remover/:codigo&:imagem', function(req, res){
+//     servico.removerProduto(req, res);
+// });
     
-   // Rota para redirecionar para o formulário de alteração/edição
-router.get('/formularioEditar/:codigo', function(req, res){
-    servico.formularioEditar(req, res); 
-});
+//    // Rota para redirecionar para o formulário de alteração/edição
+// router.get('/formularioEditar/:codigo', function(req, res){
+//     servico.formularioEditar(req, res); 
+// });
 
-// Rota para editar produtos
-router.get('/editar', function(req, res){
-    servico.editarProduto(req, res);
-});
+// // Rota para editar produtos
+// router.get('/editar', function(req, res){
+//     servico.editarProduto(req, res);
+// });
 // Exportar o router
 module.exports = router;
